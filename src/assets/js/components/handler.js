c3368be7c2cf7cec7b58a7f4ba23f21b35fc04e1
rebron1900 <me@1900.live>
2024-01-06 22:58:57 +0800

    fix

--- src/assets/js/components/handler.js
+++ src/assets/js/components/handler.js
@@ -4,13 +4,13 @@ export default function eventHandler() {
 
 function handleClick(e) {
   if (e.target.closest(".bt-menu")) {
-    document.querySelector(".menu").classList.add("menu-active");
+    document.querySelector(".menu").classList.add("menu-isactive");
     document.querySelector("body").classList.add("menu-actived");
     return;
   }
 
   if (e.target.closest(".menu")) {
-    document.querySelector(".menu").classList.remove("menu-active");
+    document.querySelector(".menu").classList.remove("menu-isactive");
     document.querySelector("body").classList.remove("menu-actived");
     return;
   }
