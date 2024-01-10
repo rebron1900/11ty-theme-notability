a06a5e8abb3546fa9255706bb4240e562b8b61a4
rebron1900 <me@1900.live>
2024-01-10 09:25:24 +0800

    fix taxi

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   envUrls: {
     development: 'http://localhost:3000',
-    staging: 'https://www.1900.live',
-    production: 'https://www.1900.live',
+    staging: 'https://1900.live',
+    production: 'https://1900.live',
     cdnUrl: 'https://cdn.1900.live',
     devData: '40',
     proData: 'all'
