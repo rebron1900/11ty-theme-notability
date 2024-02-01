65c33b2b25afd65365537c91a0479792802d2cdc
rebron1900 <me@1900.live>
2024-02-01 21:38:56 +0800

    fix nav url

--- eleventy/filters/absoluteUrl.js
+++ eleventy/filters/absoluteUrl.js
@@ -7,5 +7,9 @@ const homeUrl = envUrls[process.env.NODE_ENV.trim()]
 
 
 module.exports = (value) => {
+  const regex = /^(http|https):\/\//i;
+  if(regex.test(value)){
+    return value;
+  }
   return homeUrl ? homeUrl + value : value;
 }
